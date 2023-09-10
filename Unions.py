a981b9822b8d5b3750fb5d7ffbf3605e8a990fa5
AdmiralAndy <143858510+AdmiralAndy@users.noreply.github.com>
2023-09-10 18:58:35 -0400

    Add files via upload

---
+++ Unions.py
@@ -0,0 +1,13 @@
+My_Favourite_Foods={'hotdogs','potatos','chicken wings','salad','peanuts','hamburgers'}
+Partners_Favourite_Foods={'salads','chicken wings','hotdogs','pickles','cheesewiz','chicken legs','hamburgers'}
+
+Both_our_Favourites= My_Favourite_Foods|Partners_Favourite_Foods
+print(Both_our_Favourites)
+
+#####ALTERNATIVLEY CONCATENATE LISTS WITH + OPERATOR#####
+
+My_Favourite_Foods=['hotdogs','potatos','chicken wings','salad','peanuts','hamburgers']
+Partners_Favourite_Foods=['salads','chicken wings','hotdogs','pickles','cheesewiz','chicken legs','hamburgers']
+
+Both_our_Favourites= My_Favourite_Foods+Partners_Favourite_Foods
+print(Both_our_Favourites)
